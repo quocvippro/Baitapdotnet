ca24d70a84f62564027fba2b9db5922b75859a33
Quoc <lamquoca3k12@gmail.com>
2023-09-14 22:44:33 +0700

    Program.cs

---
+++ Timphantu/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static int Solont2(int[] arr)
+    {
+        if (arr.Length < 2)
+        {
+            Console.WriteLine("Mang phai lon hon 2 phan tu");
+            return -1;
+        }
+
+        int solonnhat = arr[0];
+        int solonthu2 = arr[0];
+
+        foreach (int num in arr)
+        {
+            if (num > solonnhat)
+            {
+                solonthu2 = solonnhat;
+                solonnhat = num;
+            }
+            else if (num > solonthu2 && num != solonnhat)
+            {
+                solonthu2 = num;
+            }
+        }
+
+        if (solonthu2 == solonnhat)
+        {
+            Console.WriteLine("Khong tim thay");
+            return -1;
+        }
+
+        return solonthu2;
+    }
+
+    static void Main()
+    {
+        Console.Write("Nhap So Luong Phan Tu mang: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(" Nhap phan tu thu" + i + " :");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        int result = Solont2(arr);
+
+        if (result != -1)
+        {
+            Console.WriteLine("Phan tu lon thu 2: " + result);
+        }
+    }
+}
---
+++ tinhtong/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tong = 0;
+        int so = 0;
+        int b = 0;
+        Console.Write("Nhap n : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            b = i;
+            String nn = b.ToString() + b.ToString();
+            so = Convert.ToInt32(nn);
+            tong += so;
+
+        }
+        Console.WriteLine("Tong la: " + tong);
+    }
+}
